0433f94bcee27dfca8ec6a7d927787339d14cfbd
Popsicool <akinolasamson1234@gmail.com>
2022-09-19 14:13:30 +0000

    update task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+/**
+ * check_cycle - check if a singly list contains circle
+ * @list: pointer to the list to check
+ * Return: 1 if there is a circle or 0 if not
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *first;
+	listint_t *secound;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+	first = list->next;
+	secound = list->next->next;
+
+	while (first && secound && secound->next)
+	{
+		if (first == secound)
+			return (1);
+
+		first = first->next;
+		secound = secound->next->next;
+	}
+	return (0);
+
+}

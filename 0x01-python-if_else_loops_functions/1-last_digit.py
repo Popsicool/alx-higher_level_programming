f3ed4dcf0ea5b4eec0448aeb723b249e8fc1e8aa
Popsicool <akinolasamson1234@gmail.com>
2022-09-20 12:10:26 +0000

    fix errors

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,11 +11,11 @@ last_num = (number % 10) * state
 number = number * state
 
 if (last_num == 0):
-    print("Last digit of"
+    print("Last digit of "
           "{} is {} and 0".format(number, last_num))
 elif (last_num > 5):
-    print("Last digit of"
+    print("Last digit of "
           "{} is {} and is greater than 5 and not 0".format(number, last_num))
 else:
-    print("Last digit of"
+    print("Last digit of "
           "{} is {} and is less than 6 and not 0".format(number, last_num))

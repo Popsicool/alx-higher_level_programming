8efab757d2cc5850fce21870e9790116a0645f6e
Popsicool <akinolasamson1234@gmail.com>
2022-09-20 12:18:05 +0000

    fix task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def uppercase(word):
-    count = 0
     for i in word:
         if (ord(i) in range(97, 123)):
             i = chr(ord(i) - 32)
         print("{}".format(i), end="")
-        count += 1
-        if (count == len(word)):
-            print("\n")
+    print("\n")

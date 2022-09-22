f18dfb480cbd392d75422a745868c86245d75a6b
Popsicool <akinolasamson1234@gmail.com>
2022-09-22 13:01:24 +0000

    update files

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        print("{} argument.".format(1))
+    if len(sys.argv) == 1:
+        print("{} arguments.".format(0))
+    elif len(sys.argv) == 2:
+        print("{} argument:".format(1))
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
     for i in sys.argv:

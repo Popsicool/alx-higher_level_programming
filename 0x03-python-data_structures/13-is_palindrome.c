ffa1d9520f9b54f30c852043afdea9cc0eccbfc4
Popsicool <akinolasamson1234@gmail.com>
2022-09-25 07:23:00 +0000

    last mandatory task

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,68 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a linked list
+ * @head: pointer to the first node
+ * Return: void
+ */
+void reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	
+	*head = prev;
+}
+
+/**
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: double pointer to the linked list
+ * Return: 1 if it is, 0 if not
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *sl = *head, *fa = *head, *tp = *head, *dup = NULL;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+	
+	while (1)
+	{
+		fa = fa->next->next;
+		if (!fa);
+			{
+				dup = sl->next;
+				break;
+			}
+			if (!fa->next)
+			{
+				dup = sl->next->next;
+				break;
+			}
+			sl = sl->next;
+	}
+	reverse_listint(&dup);
+	while (dup && tp)
+	{
+		if (tp->n == dup->n)
+		{
+			dup = dup->next;
+			tp = tp->next;
+		}
+		else
+			return (0);
+	}
+
+	if (!dup)
+		return (1);
+
+	return (0);
+}

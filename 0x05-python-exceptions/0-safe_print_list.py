9a8207a910de0e86e5cb59079aff4f246c5d7833
Popsicool <akinolasamson1234@gmail.com>
2022-10-10 12:22:24 +0000

    fix bugs

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        count = 0
-        while (count < x and count < len(my_list)):
+    count = 0
+    while (count < x and count < len(my_list)):
+        try:
             print(my_list[count], end="")
             count += 1
-        print()
-        return (count)
-    except (RuntimeError, TypeError, NameError, IndexError):
-        pass
+        except (RuntimeError, TypeError, NameError, IndexError):
+            pass
+    print()
+    return (count)
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,10 +4,10 @@ def safe_print_list_integers(my_list=[], x=0):
     leng = 0
     while (leng < x):
         try:
-            print("{:d}".format(my_list[len]), end="")
+            print("{:d}".format(my_list[leng]), end="")
             count += 1
             leng += 1
-        except (TypeError, IndexError, ValueError):
+        except (TypeError, ValueError):
             leng += 1
     print()
     return (count)

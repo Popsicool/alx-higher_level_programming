1b11326cafbca3e0f9a46338e2f9d94af5a9a8f4
Popsicool <akinolasamson1234@gmail.com>
2022-10-10 12:26:23 +0000

    fix task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    while (count < x and count < len(my_list)):
+    lent = 0
+    while (lent < x):
         try:
             print(my_list[count], end="")
             count += 1
+            lent += 1
         except (RuntimeError, TypeError, NameError, IndexError):
-            pass
+            lent += 1
     print()
     return (count)

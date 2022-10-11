7da59c749b361479a0b842659143e8fb839e00e2
Popsicool <akinolasamson1234@gmail.com>
2022-10-11 11:40:54 +0000

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
+""" Declare class square"""
+
+
 class Square:
-	def __init__(self, size=0):
-		if (isinstance(size, int) == False):
-			raise TypeError('size must be an integer')
-		elif (size < 0):
-			raise ValueError('size must be >= 0')
-		else:
-			self.__size = size
-	@property	
-	def size(self):
-		return self.__size
-		
-	@size.setter
-	def size(self, value):
-		if (isinstance(value, int) == False):
-			raise TypeError('size must be an integer')
-		elif (value < 0):
-			raise ValueError('size must be >= 0')
-		else:
-			self.__size = value
-			
-	def area(self):
-		return self.__size ** 2
+    """initialize class"""
+    def __init__(self, size=0):
+        if (isinstance(size, int) is False):
+            raise TypeError('size must be an integer')
+        elif (size < 0):
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+        """set size method"""
+        @property
+        def size(self):
+            return self.__size
+        """ define setter for size"""
+        @size.setter
+        def size(self, value):
+            if (isinstance(value, int) is False):
+                raise TypeError('size must be an integer')
+            elif (value < 0):
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = value
+        """ area method"""
+        def area(self):
+            return self.__size ** 2

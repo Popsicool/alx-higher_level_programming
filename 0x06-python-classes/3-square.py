fe578d6168b046237261539610fecaa54a17f7fa
Popsicool <akinolasamson1234@gmail.com>
2022-10-11 11:33:01 +0000

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+""" Declare class square"""
+
+
 class Square:
-	def __init__(self, size=0):
-		if (isinstance(size, int) == False):
-			raise TypeError('size must be an integer')
-		elif (size < 0):
-			raise ValueError('size must be >= 0')
-		else:
-			self.__size = size
-	def area(self):
-		return self.__size ** 2
-		
+    """Initialize class square"""
+    def __init__(self, size=0):
+        if (isinstance(size, int) is False):
+            raise TypeError('size must be an integer')
+        elif (size < 0):
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+    """ Declare area method"""
+    def area(self):
+        ireturn self.__size ** 2

961c1f822de1af0d1bc6259d4efb10ed89450a90
Popsicool <akinolasamson1234@gmail.com>
2022-10-11 11:48:05 +0000

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -12,18 +12,18 @@ class Square:
         else:
             self.__size = size
         """set size method"""
-        @property
-        def size(self):
-            return self.__size
-        """ define setter for size"""
-        @size.setter
-        def size(self, value):
-            if (isinstance(value, int) is False):
-                raise TypeError('size must be an integer')
-            elif (value < 0):
-                raise ValueError('size must be >= 0')
-            else:
-                self.__size = value
-        """ area method"""
-        def area(self):
-            return self.__size ** 2
+    @property
+    def size(self):
+        return self.__size
+    """ define setter for size"""
+    @size.setter
+    def size(self, value):
+        if (isinstance(value, int) is False):
+            raise TypeError('size must be an integer')
+        elif (value < 0):
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+    """ area method"""
+    def area(self):
+        return self.__size ** 2

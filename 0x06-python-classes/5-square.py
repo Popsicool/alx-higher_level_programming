8fb524bb54c4fed8867b5ce13c97cce056f7314c
Popsicool <akinolasamson1234@gmail.com>
2022-10-11 11:59:17 +0000

    task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,37 +1,42 @@
 #!/usr/bin/python3
+"""define a class square"""
+
+
 class Square:
-	def __init__(self, size=0):
-		if (isinstance(size, int) == False):
-			raise TypeError('size must be an integer')
-		elif (size < 0):
-			raise ValueError('size must be >= 0')
-		else:
-			self.__size = size
-	@property	
-	def size(self):
-		return self.__size
-		
-	@size.setter
-	def size(self, value):
-		if (isinstance(value, int) == False):
-			raise TypeError('size must be an integer')
-		elif (value < 0):
-			raise ValueError('size must be >= 0')
-		else:
-			self.__size = value
-			
-	def area(self):
-		return self.__size ** 2
-	
-	def my_print(self):
-		if self.__size == 0:
-			print()
-			return
-		i = 0
-		while ( i < self.__size):
-			j = 0
-			while (j < self.__size):
-				print("#", end="")
-				j += 1
-			print()
-			i += 1
+    """Initialize square class"""
+    def __init__(self, size=0):
+        if (isinstance(size, int) is False):
+            raise TypeError('size must be an integer')
+        elif (size < 0):
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+    """Retrive size property"""
+    @property
+    def size(self):
+        return self.__size
+    """ Set size property"""
+    @size.setter
+    def size(self, value):
+        if (isinstance(value, int) is False):
+            raise TypeError('size must be an integer')
+        elif (value < 0):
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+    """ Area method"""
+    def area(self):
+        return self.__size ** 2
+    """ Print square"""
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        return
+        i = 0
+        while (i < self.__size):
+            j = 0
+            while (j < self.__size):
+                print("#", end="")
+                j += 1
+            print()
+            i += 1

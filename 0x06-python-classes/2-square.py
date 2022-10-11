90f1327deef392cf3374034addfa0252c8f5cf32
Popsicool <akinolasamson1234@gmail.com>
2022-10-11 11:28:50 +0000

    task2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,8 +4,8 @@
 
 class Square:
     """ initialize the square"""
-    def __init__(self, size = 0):
-        if (isinstance(size, int) == False):
+    def __init__(self, size=0):
+        if (isinstance(size, int) is False):
             raise TypeError('size must be an integer')
         elif (size < 0):
             raise ValueError('size must be >= 0')

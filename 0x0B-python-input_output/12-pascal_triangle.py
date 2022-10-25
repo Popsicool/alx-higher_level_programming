c7e827a85f593fabf303ae43f6f678575eb12725
Popsicool <akinolasamson1234@gmail.com>
2022-10-25 11:36:06 +0000

    removed all main files

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -11,11 +11,11 @@ def pascal_triangle(n):
 
     triangles = [[1]]
 
-    while (len(triangle) != n):
+    while (len(triangles) != n):
         triangle = triangles[-1]
         tmp = [1]
         for i in range(len(triangle) - 1):
             tmp.append(triangle[i] + triangle[i + 1])
         tmp.append(1)
         triangles.append(tmp)
-    return (triangle)
+    return (triangles)

84e0e4bba000b81b26cf100e8768044fca199df9
Popsicool <akinolasamson1234@gmail.com>
2022-12-20 12:06:52 +0100

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,16 +4,11 @@ const all = process.argv;
 if (all.length < 4) {
   console.log(0);
 } else {
-  let highest = 0;
-  let second = 0;
+  const a = [];
   for (let i = 2; parseInt(all[i]); i++) {
-    if (i === 2) {
-      highest = parseInt(all[i]);
-      second = parseInt(all[i]);
-    } else if (parseInt(all[i]) >= highest) {
-      second = highest;
-      highest = parseInt(all[i]);
-    }
+    a.push(parseInt(all[i]));
   }
-  console.log(second);
+  a.sort();
+  a.reverse();
+  console.log(a[1]);
 }

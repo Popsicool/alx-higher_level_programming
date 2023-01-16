2226390450f38f493f35d04a651240d245e3f0d8
Popsicool <akinolasamson1234@gmail.com>
2023-01-16 12:18:09 +0100

    fix task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sX DELETE "$1"
+curl -si -L -X OPTIONS "$1" | grep "Allow" | cut -d " " -f 2-

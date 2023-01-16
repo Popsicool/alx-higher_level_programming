38c901e1d8400df618fcff1b725fd0993ced7437
Popsicool <akinolasamson1234@gmail.com>
2023-01-16 11:20:10 +0100

    initial commit

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def find_peak(list_of_integers):
+    """
+    function that finds a peak in a list of unsorted integers.
+    """
+    if len(list_of_integers) > 1:
+        return max(list_of_integers)
+    return

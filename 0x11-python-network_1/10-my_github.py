2200949a97aaa5e2af3de2a0943d00a83f97e851
Popsicool <akinolasamson1234@gmail.com>
2023-01-17 10:51:06 +0100

    all mandatory tasks

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes your GitHub credentials
+(username and password) and uses the GitHub API to display your id
+"""
+
+import requests
+import sys
+
+username = sys.argv[1]
+password = sys.argv[2]
+auth = requests.auth.HTTPBasicAuth(username, password)
+r = requests.get("https://api.github.com/user", auth=auth)
+print(r.json().get("id"))
---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+ Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+import requests
+
+r = requests.get("https://alx-intranet.hbtn.io/status")
+
+print("Body response:")
+print("\t- type: {}".format(type(r.text)))
+print("\t- content: {}".format(r.text))
